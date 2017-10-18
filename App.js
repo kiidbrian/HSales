8c5b87b8a4d16ddca8a7d651d46b1157e405019b
kiidbrian <brnpaintsil@gmail.com>
2017-10-18 00:39:08 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          raised
+          icon={{name: 'home', size: 32}}
+          buttonStyle={{backgroundColor: 'red', borderRadius: 10}}
+          textStyle={{textAlign: 'center'}}
+          title={`Welcome to\nHappy Sales App`}
+         />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
